94bcb27291a8c1e5fb27c4be4c2c7e4818dfa41a
Dominik Gersak <dgersak@sk.ibm.com>
2017-05-16 13:15:32 +0200

    fixed parking rendering (included formatting)

--- src/js/game/fields/ParkingField.js
+++ src/js/game/fields/ParkingField.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import MoneyInlineHelper from '../../utils/helpers/Money';
 
 class ParkingField extends React.Component {
 
     render () {
         return <div>
-            { this.props.parkingMoney }
+            <MoneyInlineHelper amount={ this.props.parkingMoney } />
         </div>;
     }
 }
